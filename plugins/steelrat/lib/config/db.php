87f24deea129ae5b4a0ad030ec714397cc6f1952
Andrew Godin <covoxx@gmail.com>
2018-06-01 15:59:28 +0300

    исправлена ошибка в имени таблицы

--- plugins/steelrat/lib/config/db.php
+++ plugins/steelrat/lib/config/db.php
@@ -8,7 +8,7 @@ return array(
             'PRIMARY' => 'id',
         ),
     ),
-    'telegram_steelrat_users' => array(
+    'telegram_steelrat_user' => array(
         'chat_id' => array('int', 11, 'null' => 0),
         'name' => array('varchar', 50, 'null' => 0),
         'book_id' => array('int', 11),
