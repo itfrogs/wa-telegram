2db2bac5a0c9f548774b45b4a40df286b5c4afc7
Andrew Godin <covoxx@gmail.com>
2023-10-21 14:29:14 +0300

    смена версии

--- plugins/steelrat/lib/config/plugin.php
+++ plugins/steelrat/lib/config/plugin.php
@@ -2,7 +2,7 @@
 return array (
     'name' => _wp('You can be the Stainless Steel Rat'),
     'img' => 'img/steelrat16.png',
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'vendor' => '964801',
     'frontend' => true,
     'handlers' =>
